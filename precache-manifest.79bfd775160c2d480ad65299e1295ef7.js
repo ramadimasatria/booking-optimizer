05ff4ff6428edfdf96f6158fc0a1a7cdc16d1306
Rama Dimasatria <ramadimasatria@gmail.com>
2019-07-14 21:41:20 +0700

    Updates

---
+++ precache-manifest.79bfd775160c2d480ad65299e1295ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995ae89f47618485a270bcb54bdf5ef6",
+    "url": "/booking-optimizer/index.html"
+  },
+  {
+    "revision": "517322406a1bbc48fac5",
+    "url": "/booking-optimizer/static/css/main.b8f222d9.chunk.css"
+  },
+  {
+    "revision": "df70b07581b2f5cd26e4",
+    "url": "/booking-optimizer/static/js/2.e647c3e3.chunk.js"
+  },
+  {
+    "revision": "517322406a1bbc48fac5",
+    "url": "/booking-optimizer/static/js/main.bd7adf1f.chunk.js"
+  },
+  {
+    "revision": "8863be23c4d383ec8f45",
+    "url": "/booking-optimizer/static/js/runtime~main.d72cbe76.js"
+  },
+  {
+    "revision": "279196e37844ff702af163035a67c609",
+    "url": "/booking-optimizer/static/media/right-arrow.279196e3.svg"
+  }
+]);
