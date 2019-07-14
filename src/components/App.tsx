0f8adb83d9c820a520e46812f906cf1804cfb329
Rama Dimasatria <ramadimasatria@gmail.com>
2019-07-14 21:11:25 +0700

    Optimize button functionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,55 +1,104 @@
 import React from 'react';
-import { Booking } from '../types';
-import BookingOrder from './BookingOrder';
 // @ts-ignore
 import JSONInput from 'react-json-editor-ajrm';
+import BookingOrder from './BookingOrder';
+
+import { Booking } from '../types';
+import BookingManager from '../booking-manager';
+import rawData from '../data.json';
+
 import './App.scss';
 
-export interface AppProps {
-  original: Booking[],
-  optimized: Booking[],
-  originalRelocations: number,
-  optimizedRelocations: number
+interface AppState {
+  isLoading: boolean,
+  original?: Booking[],
+  optimized?: Booking[],
+  originalRelocations?: number,
+  optimizedRelocations?: number
 }
 
-const App: React.FC<AppProps> = (props) => {
-  return (
-    <div className="app">
-      <h1 className="app__title">Booking Order Optimizer</h1>
+const bookingManager = new BookingManager()
 
-      <div className="app_section">
-        <h3 className="app__subtitle">Input Data</h3>
+class App extends React.Component<{}, AppState> {
+  constructor(props: any) {
+    super(props);
 
-        <JSONInput
-          id='editor'
-          placeholder={props.original}
-          height='400px'
-          theme='light_mitsuketa_tribute'
-          width='100%'
-        />
+    this.state = {
+      isLoading: false
+    }
 
-        <p style={{ textAlign: 'center' }}>
-          <button className="button" style={{ marginTop: 18 }}>Optimize!</button>
-        </p>
+    this.optimize = this.optimize.bind(this)
+  }
+
+  optimize() {
+    this.setState({
+      isLoading: true
+    })
+
+    bookingManager.optimize(rawData)
+
+    this.setState({
+      isLoading: false,
+      original: bookingManager.original,
+      optimized: bookingManager.optimized,
+      originalRelocations: bookingManager.originalRelocations,
+      optimizedRelocations: bookingManager.optimizedRelocations
+    })
+  }
+
+  render() {
+    const { state } = this
+
+    return (
+      <div className="app">
+        <h1 className="app__title">Booking Order Optimizer</h1>
+
+        <div className="app_section">
+          <h3 className="app__subtitle">Input Data</h3>
+
+          <JSONInput
+            id='editor'
+            placeholder={ rawData }
+            height='400px'
+            theme='light_mitsuketa_tribute'
+            width='100%'
+          />
+
+          <p style={{ textAlign: 'center' }}>
+            <button className="button" style={{ marginTop: 18 }} onClick={this.optimize} disabled={state.isLoading}>
+              Optimize!
+            </button>
+          </p>
+        </div>
+
+        <hr className="app_separator" />
+
+        {
+          state.original && (
+            <React.Fragment>
+              <div className="app__section">
+                <h3 className="app__subtitle">Original Order</h3>
+                <BookingOrder order={state.original} />
+                <p>Relocations: { state.originalRelocations }</p>
+              </div>
+
+              <hr className="app__separator" />
+            </React.Fragment>
+          )
+        }
+
+        {
+          state.optimized && (
+            <div className="app__section">
+              <h3 className="app__subtitle">Optimized Order</h3>
+              <BookingOrder order={ state.optimized } />
+              <p>Relocations: { state.optimizedRelocations }</p>
+            </div>
+          )
+        }
       </div>
-
-      <hr className="app_separator" />
-
-      <div className="app__section">
-        <h3 className="app__subtitle">Original Order</h3>
-        <BookingOrder order={props.original} />
-        <p>Relocations: { props.originalRelocations }</p>
-      </div>
-
-      <hr className="app__separator" />
-
-      <div className="app__section">
-        <h3 className="app__subtitle">Optimized Order</h3>
-        <BookingOrder order={props.optimized} />
-        <p>Relocations: { props.optimizedRelocations }</p>
-      </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import rawData from './data.json';
-import BookingManager from './booking-manager';
-import App, { AppProps } from './components/App';
-
-const bookingManager = new BookingManager()
-bookingManager.optimize(rawData)
-
-const props: AppProps = {
-  original: bookingManager.original,
-  optimized: bookingManager.optimized,
-  originalRelocations: bookingManager.originalRelocations,
-  optimizedRelocations: bookingManager.optimizedRelocations
-}
+import App from './components/App';
 
 ReactDOM.render(
-  <App {...props} />
+  <App/>
   , document.getElementById('root'));
